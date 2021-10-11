68992e0ee8929199235c928a18801894391d0dc0
Benoit Daireaux <beda@norceresearch.no>
2021-10-11 10:02:13 +0200

    Second draft

--- OSDC.YPL.ModelCalibration.FromRheometer/OSDC.YPL.RheometerCorrection/ShearRateCorrection.cs
+++ OSDC.YPL.ModelCalibration.FromRheometer/OSDC.YPL.RheometerCorrection/ShearRateCorrection.cs
@@ -1,8 +1,14 @@
 ﻿using OSDC.YPL.ModelCalibration.FromRheometer.Model;
 
-
 namespace OSDC.YPL.RheometerCorrection
 {
     public static class ShearRateCorrection
-    { }
+    {
+        public static bool CorrectShearRate(Rheogram rheogram)
+        {
+            return true;
+        }
+    
+    
+    }
 }
