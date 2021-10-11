814d5b6cfc4a78ef31a12b60f69ec5b2e85ab253
Benoit Daireaux <beda@norceresearch.no>
2021-10-11 10:27:55 +0200

    Skeleton of methods

--- OSDC.YPL.ModelCalibration.FromRheometer/OSDC.YPL.RheometerCorrection/ShearRateCorrection.cs
+++ OSDC.YPL.ModelCalibration.FromRheometer/OSDC.YPL.RheometerCorrection/ShearRateCorrection.cs
@@ -4,8 +4,22 @@ namespace OSDC.YPL.RheometerCorrection
 {
     public static class ShearRateCorrection
     {
-        public static bool CorrectShearRate(Rheogram rheogram)
+        /// <summary>
+        ///	Skadsem and Saasen(2019) :
+        ///	Concentric cylinder viscometer flows ofHerschel-Bulkley fluids
+        ///	
+        /// Solving the Couette inverse problem
+        ///using a wavelet-vaguelette decomposition
+        ///Christophe Ancey
+        /// </summary>
+        /// <param name="uncorrectedRheogram"></param>
+        /// <param name="correctedRheogram"></param>
+        /// <returns></returns>
+    public static bool CorrectShearRate(Rheogram uncorrectedRheogram, out Rheogram correctedRheogram)
         {
+            correctedRheogram = new Rheogram();
+
+
             return true;
         }
     
--- OSDC.YPL.ModelCalibration.FromRheometer/OSDC.YPL.RheometerCorrection/ShearStressCorrection.cs
+++ OSDC.YPL.ModelCalibration.FromRheometer/OSDC.YPL.RheometerCorrection/ShearStressCorrection.cs
@@ -5,8 +5,22 @@ namespace OSDC.YPL.RheometerCorrection
 {
     public static class ShearStressCorrection
     {
-        public static bool CorrectShearStress(Rheogram rheogram)
+        /// <summary>
+        /// Non-Newtonian end-effects in standard oilfield rheometers
+        /// Etienne Lac and Andrew Parry
+        /// 
+        /// 
+        /// Solving the Couette inverse problem
+        /// using a wavelet-vaguelette decomposition
+        /// Christophe Ancey
+        /// <param name="uncorrectedRheogram"></param>
+        /// <param name="correctedRheogram"></param>
+        /// <returns></returns>
+        public static bool CorrectShearStress(Rheogram uncorrectedRheogram, out Rheogram correctedRheogram)
         {
+            correctedRheogram = new Rheogram();
+
+
             return true;
         }
     }
