f9323be9f44ff93032c4e371a9b61e9d6027846c
Benoît Daireaux <54928991+benoitdaireaux@users.noreply.github.com>
2021-10-14 19:06:28 +0200

    Played with a tex extension

--- OSDC.YPL.ModelCalibration.FromRheometer/OSDC.YPL.RheometerCorrection/ShearRateCorrection.cs
+++ OSDC.YPL.ModelCalibration.FromRheometer/OSDC.YPL.RheometerCorrection/ShearRateCorrection.cs
@@ -15,14 +15,41 @@ namespace OSDC.YPL.RheometerCorrection
         /// <param name="uncorrectedRheogram"></param>
         /// <param name="correctedRheogram"></param>
         /// <returns></returns>
-    public static bool CorrectShearRate(Rheogram uncorrectedRheogram, out Rheogram correctedRheogram)
+        public static bool CorrectShearRate(Rheogram uncorrectedRheogram, out Rheogram correctedRheogram)
         {
             correctedRheogram = new Rheogram();
 
 
             return true;
         }
-    
-    
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="kappa"></param>
+        /// <param name="n"></param>
+        /// <param name="nbOfPoints"></param>
+        /// <returns></returns>
+        private static double IntegrationFKappaN(double kappa, double n, int nbOfIntervals = 100)
+        {
+            //tex: Performs the integration from equation (7):
+            //$$\int_\kappa^1 \frac{1}{\tilde r} \left(\frac{1}{\tilde{r}^2} -1   \right)^{\frac 1n}d\tilde r  $$
+
+            double step = (1.0 - kappa) / nbOfIntervals;
+            double oneOverN = 1.0 / n;
+
+            double integral = 0;
+
+            for (int i = 0; i < nbOfIntervals; i++)
+            {
+                double x_l = kappa + i * step;
+                double x_r = kappa + (i + 1) * step;
+                double y_l = (1.0 / x_l) * System.Math.Pow(1.0 / (x_l * x_l) - 1, oneOverN);
+                double y_r = (1.0 / x_r) * System.Math.Pow(1.0 / (x_r * x_r) - 1, oneOverN);
+
+                integral += (y_l + y_r) * .5 * step;
+            }
+            return integral;
+        }
     }
 }
