c7489f8b183c0e7f72ed39c8d6d8344296e2337a
GillesNorce <gipe@norceresearch.no>
2022-03-02 10:23:17 +0100

    Rearchitecturing of the microservice and webapp (based on Blazor server)

--- OSDC.YPL.ModelCalibration.FromRheometer/OSDC.YPL.ModelCalibration.FromRheometer.Service/GenerateDoc.bat
+++
@@ -1,4 +0,0 @@
-cd %1OSDC.YPL.ModelCalibration.FromRheometer.Model
-(docfx build -o %1\OSDC.YPL.ModelCalibration.FromRheometer.Service\wwwroot\YPLCalibrationFromRheometer\ModelAPI) || (echo "docfx for model failed")
-cd %1OSDC.YPL.ModelCalibration.FromRheometer.Service
-(docfx build -o %1\OSDC.YPL.ModelCalibration.FromRheometer.Service\wwwroot\YPLCalibrationFromRheometer\ServiceAPI) || (echo "docfx for service failed")
